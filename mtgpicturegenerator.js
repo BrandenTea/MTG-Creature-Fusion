05e8f8c1e08c1ca321f74b9d378b1d9933acd813
BrandenTea <98258615+BrandenTea@users.noreply.github.com>
2022-03-15 20:58:19 -0500

    Add files via upload

---
+++ mtgpicturegenerator.js
@@ -0,0 +1,459 @@
+let mtgGreenCreaturePictureGrid = ['greenTopLeft','greenTopMiddle','greenTopRight',
+        'greenMiddleLeft','greenMiddleMiddle','greenMiddleRight',
+        'greenBottomLeft','greenBottomMiddle','greenBottomRight'        
+        
+];
+        
+let mtgWhiteCreaturePictureGrid = ['whiteTopLeft','whiteTopMiddle','whiteTopRight',
+'whiteMiddleLeft','whiteMiddleMiddle','whiteMiddleRight',
+'whiteBottomLeft','whiteBottomMiddle','whiteBottomRight'        
+
+];
+
+let mtgRedCreaturePictureGrid = ['redTopLeft','redTopMiddle','redTopRight',
+'redMiddleLeft','redMiddleMiddle','redMiddleRight',
+'redBottomLeft','redBottomMiddle','redBottomRight'        
+
+];
+
+
+/*Garruk Section*/
+
+function show() {
+ 
+        
+        document.getElementById('garruk3TopLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonTopLeft')
+                .style.display = "none";
+
+                document.getElementById('blueButtonTopLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopLeft')
+                .style.display = "none";  
+}
+
+
+function showGreenTopMiddle() {
+ 
+        
+        document.getElementById('garruk3TopMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonTopMiddle')
+                .style.display = "none";
+
+                document.getElementById('blueButtonTopMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopMiddle')
+                .style.display = "none";  
+}
+
+function showGreenTopRight() {
+ 
+        
+        document.getElementById('garruk3TopRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonTopRight')
+                .style.display = "none";
+
+                document.getElementById('blueButtonTopRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopRight')
+                .style.display = "none";  
+}
+
+function showGreenMiddleLeft() {
+ 
+        
+        document.getElementById('garruk3MiddleLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonMiddleLeft')
+                .style.display = "none";
+
+                document.getElementById('blueButtonMiddleLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddleLeft')
+                .style.display = "none";  
+}
+
+function showGreenMiddle() {
+ 
+        
+        document.getElementById('garruk3MiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonMiddle')
+                .style.display = "none";
+
+                document.getElementById('blueButtonMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddle')
+                .style.display = "none";  
+}
+
+function showGreenMiddleRight() {
+ 
+        
+        document.getElementById('garruk3MiddleRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonMiddleRight')
+                .style.display = "none";
+
+                document.getElementById('blueButtonMiddleRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddleRight')
+                .style.display = "none";  
+}
+
+function showGreenBottomLeft() {
+ 
+        
+        document.getElementById('garruk3BottomLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonBottomLeft')
+                .style.display = "none";
+
+                document.getElementById('blueButtonBottomLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomLeft')
+                .style.display = "none";  
+}
+
+function showGreenBottomMiddle() {
+ 
+        
+        document.getElementById('garruk3BottomMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonBottomMiddle')
+                .style.display = "none";
+
+                document.getElementById('blueButtonBottomMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomMiddle')
+                .style.display = "none";  
+}
+
+function showGreenBottomRight() {
+ 
+        
+        document.getElementById('garruk3BottomRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('greenButtonBottomRight')
+                .style.display = "none";
+
+                document.getElementById('blueButtonBottomRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomRight')
+                .style.display = "none";  
+}
+
+
+/*Jace Section*/
+
+function showBlueTopMiddle() {
+ 
+        
+        document.getElementById('jace3TopMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonTopMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopMiddle')
+                .style.display = "none";  
+}
+
+function showBlueTopLeft() {
+ 
+        
+        document.getElementById('jace3TopLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonTopLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopLeft')
+                .style.display = "none";  
+}
+
+function showBlueTopRight() {
+ 
+        
+        document.getElementById('jace3TopRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonTopRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonTopRight')
+                .style.display = "none";
+}
+
+function showBlueMiddle() {
+ 
+        
+        document.getElementById('jace3MiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddle')
+                .style.display = "none";  
+}
+
+function showBlueMiddleLeft() {
+ 
+        
+        document.getElementById('jace3MiddleLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonMiddleLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddleLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddleLeft')
+                .style.display = "none";  
+}
+
+function showBlueMiddleRight() {
+ 
+        
+        document.getElementById('jace3MiddleRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonMiddleRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddleRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonMiddleRight')
+                .style.display = "none";  
+}
+
+function showBlueBottomLeft() {
+ 
+        
+        document.getElementById('jace3BottomLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonBottomLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomLeft')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomLeft')
+                .style.display = "none";  
+}
+
+function showBlueBottomMiddle() {
+ 
+        
+        document.getElementById('jace3BottomMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonBottomMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomMiddle')
+                .style.display = "none";  
+}
+
+function showBlueBottomRight() {
+ 
+        
+        document.getElementById('jace3BottomRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('blueButtonBottomRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomRight')
+                .style.display = "none";  
+                
+                document.getElementById('colorlessButtonBottomRight')
+                .style.display = "none";  
+}
+
+/* Karn Section */
+
+
+function showColorlessTopMiddle() {
+ 
+        
+        document.getElementById('karn3TopMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonTopMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonTopMiddle')
+                .style.display = "none";  
+}
+
+function showColorlessTopLeft() {
+ 
+        
+        document.getElementById('karn3TopLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonTopLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopLeft')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonTopLeft')
+                .style.display = "none";  
+}
+
+function showColorlessTopRight() {
+ 
+        
+        document.getElementById('karn3TopRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonTopRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonTopRight')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonTopRight')
+                .style.display = "none";
+}
+
+function showColorlessMiddle() {
+ 
+        
+        document.getElementById('karn3MiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonMiddle')
+                .style.display = "none";  
+}
+
+function showColorlessMiddleLeft() {
+ 
+        
+        document.getElementById('karn3MiddleLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonMiddleLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddleLeft')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonMiddleLeft')
+                .style.display = "none";  
+}
+
+function showColorlessMiddleRight() {
+ 
+        
+        document.getElementById('karn3MiddleRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonMiddleRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonMiddleRight')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonMiddleRight')
+                .style.display = "none";  
+}
+
+function showColorlessBottomLeft() {
+ 
+        
+        document.getElementById('karn3BottomLeftDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonBottomLeft')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomLeft')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonBottomLeft')
+                .style.display = "none";  
+}
+
+function showColorlessBottomMiddle() {
+ 
+        
+        document.getElementById('karn3BottomMiddleDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonBottomMiddle')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomMiddle')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonBottomMiddle')
+                .style.display = "none";  
+}
+
+function showColorlessBottomRight() {
+ 
+        
+        document.getElementById('karn3BottomRightDisplay')
+                .style.display = "block";
+
+        document.getElementById('colorlessButtonBottomRight')
+                .style.display = "none";
+
+                document.getElementById('greenButtonBottomRight')
+                .style.display = "none";  
+                
+                document.getElementById('blueButtonBottomRight')
+                .style.display = "none";  
+}
